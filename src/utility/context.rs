025331a46d627e5b792b739bbdc14aa6088ab3d9
wangyan <381433852@qq.com>
2020-04-26 16:34:34 +0800

    Update context.rs

--- src/utility/context.rs
+++ src/utility/context.rs
@@ -1,5 +1,8 @@
 use std::any::Any;
 use std::collections::HashMap;
+use futures::future::{ok, Ready};
+use actix_web::dev::Payload;
+use actix_web::{Error, FromRequest, HttpRequest};
 
 pub struct Context {
     map: HashMap<String, Box<dyn Any>>
@@ -17,11 +20,29 @@ impl Context {
     }
 
     pub fn get<T: 'static>(&mut self, key: impl AsRef<str>) -> Option<&T> {
-        self.map.get(key.as_ref()).and_then(|boxed| (&**boxed as &(dyn Any + 'static)).downcast_ref())
+        self.map.get(key.as_ref())
+            .and_then(|boxed| (&**boxed as &(dyn Any + 'static)).downcast_ref())
     }
 
     pub fn get_mut<T: 'static>(&mut self, key: impl AsRef<str>) -> Option<&mut T> {
-        self.map.get_mut(key.as_ref()).and_then(|boxed| (&mut **boxed as &mut (dyn Any + 'static)).downcast_mut())
+        self.map.get_mut(key.as_ref())
+            .and_then(|boxed| (&mut **boxed as &mut (dyn Any + 'static)).downcast_mut())
+    }
+}
+
+impl FromRequest for Context
+{
+    type Error = Error;
+    type Future = Ready<Result<Self, Error>>;
+    type Config = ();
+
+    #[inline]
+    fn from_request(req: &HttpRequest, _: &mut Payload) -> Self::Future {
+        if let Some(ctx) = req.extensions_mut().remove::<Context>() {
+            ok(ctx)
+        } else {
+            ok(Context::new())
+        }
     }
 }
 
